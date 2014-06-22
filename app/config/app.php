450a0d20176245f25bf46b5a8a7c65455748bf39
juan2ramos <juan2ramos@gmail.com>
2014-06-22 15:47:59 -0500

    + configuración inicial

--- app/config/app.php
+++ app/config/app.php
@@ -39,20 +39,20 @@ return array(
 	|
 	*/
 
-	'timezone' => 'UTC',
+    'timezone' => 'America/Bogota',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Application Locale Configuration
-	|--------------------------------------------------------------------------
-	|
-	| The application locale determines the default locale that will be used
-	| by the translation service provider. You are free to set this value
-	| to any of the locales which will be supported by the application.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Application Locale Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The application locale determines the default locale that will be used
+    | by the translation service provider. You are free to set this value
+    | to any of the locales which will be supported by the application.
+    |
+    */
 
-	'locale' => 'en',
+	'locale' => 'es',
 
 	/*
 	|--------------------------------------------------------------------------
